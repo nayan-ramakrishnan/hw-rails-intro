03a171cea1413b5e10cc098fe48192afd20e82f4
Nayan Ramakrishnan <nayanr505@berkeley.edu>
2026-07-23 00:25:58 +0000

    Resetting db with key

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,5 +7,6 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
 # Run database migrations and seed data automatically on deploy
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rails db:reset
 bundle exec rails db:seed
