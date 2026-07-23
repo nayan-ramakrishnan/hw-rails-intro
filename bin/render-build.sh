34d022dded681f553220313f7e68c4cf1d23f60f
Nayan Ramakrishnan <nayanr505@berkeley.edu>
2026-07-23 00:32:07 +0000

    Removed seed, reset

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,6 +7,4 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
 # Run database migrations and seed data automatically on deploy
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-bundle exec rails db:reset
-bundle exec rails db:seed
+bundle exec rails db:migrate
