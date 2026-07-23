184b93d1e5a3d17d28652dd67e5f9faede3c7081
Nayan Ramakrishnan <nayanr505@berkeley.edu>
2026-07-23 00:36:08 +0000

    Trying again claude

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-
-# Run database migrations and seed data automatically on deploy
-bundle exec rails db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:create
+bundle exec rails db:schema:load
+bundle exec rails db:seed
